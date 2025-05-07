1b84f1cb2dfaa7eaa99c2a4895697dc17ff227af
unknown <sietkod@tcd.ie>
2025-05-07 15:30:04 +0100

    added eye-icon to show password on login form

--- codingthoughts/src/components/form/LoginForm.tsx
+++ codingthoughts/src/components/form/LoginForm.tsx
@@ -1,36 +1,34 @@
 "use client";
-import { useRouter } from "next/navigation";
 import { useState } from "react";
 import styles from "./LoginForm.module.css";
+import Image from 'next/image';
+
 
 export default function loginForm() {
     // strings for the email and password
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
 
-    const router = useRouter();
-
-    // function to go to signup page, just pushing the signup route onto router
-    const signupUser = () => {
-        router.push("/signup");
-    }
+    //useState for revealing or hiding password
+    const [reveal, setReveal] = useState<boolean>(false);
 
     const loginUser = () => {
         // will put logic to validate a user log in here
     }
 
-
-
     return (
         <form className={styles.container}>
             <header className={styles.title}>Log in Here</header>
             <div className={styles.entries}>
-                <input className={styles.input} onChange={(e) => {setEmail(e.target.value)}} placeholder="Enter email or username"></input>
-                <input className={styles.input} onChange={(e) => {setPassword(e.target.value)}} placeholder="Enter Password"></input>
-                <button className={styles.button} onClick={loginUser}>Login</button>
+                <input type="text" className={styles.input} onChange={(e) => {setEmail(e.target.value)}} placeholder="Enter email or username"></input>
+                <div className={styles.passwordEntry}>
+                    <input type={reveal ? "text" : "password"} className={styles.input} onChange={(e) => {setPassword(e.target.value)}} placeholder="Enter Password"></input>
+                    <Image className={styles.icon} onClick={() => setReveal(!reveal)} src={reveal ? "/eye/eye-show.svg" : "eye/eye-hide.svg" } width={25} height={25} alt="EYE"/>
+                </div>
+                <button type="submit" className={styles.button} onClick={loginUser}>Login</button>
             </div>
             <div className={styles.bottom}>
-                <span>If a new user:</span>
+                <span>OR</span>
                 <div className={styles.linksContainer}>
                     <a className={styles.links}>Forgot Password?</a>
                     <a className={styles.links} href="/signup" rel="Sign up">Sign up</a>
