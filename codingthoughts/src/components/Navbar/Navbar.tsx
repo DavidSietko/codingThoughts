949a6bdff46c932814590c4582d1d1bc26a91e99
unknown <mypuzzle12@gmail.com>
2025-05-09 18:44:25 +0100

    correctly layed out links in the navbar

--- codingthoughts/src/components/Navbar/Navbar.tsx
+++ codingthoughts/src/components/Navbar/Navbar.tsx
@@ -1,15 +1,15 @@
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
-    let list: string[] = ["Login", "Signup", "About", "Settings"];
+    let list: string[] = ["login", "signup", "About", "Settings"];
     return (
         <div className={styles.container}>
-            <a href="">Home</a>
-            <ul>
+            <a id={styles.homeTag} href="">Home</a>
+            <div className={styles.tags}>
                 {list.map((item, index) => (
                     <a href={`/${item}`} key={index}>{item}</a>
                 ))}
-            </ul>
+            </div>
         </div>
     );
 }
