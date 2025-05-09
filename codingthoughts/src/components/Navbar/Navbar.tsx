1a739028fd5ed9579c7587b3f0a5f6eb66df14bf
unknown <mypuzzle12@gmail.com>
2025-05-09 23:04:54 +0100

    finished navbar styling

--- codingthoughts/src/components/Navbar/Navbar.tsx
+++ codingthoughts/src/components/Navbar/Navbar.tsx
@@ -1,13 +1,20 @@
 import styles from "./Navbar.module.css";
+import Image from 'next/image';
 
 export default function Navbar() {
     let list: string[] = ["login", "signup", "About", "Settings"];
     return (
         <div className={styles.container}>
-            <a id={styles.homeTag} href="">Home</a>
+            <div className={styles.left}>
+                <Image className={styles.logo} src="/logo/lightbulb_logo1.png" width={100} height={100} alt="LOGO"></Image>
+                <a href="/">Home</a>
+            </div>
             <div className={styles.tags}>
                 {list.map((item, index) => (
-                    <a href={`/${item}`} key={index}>{item}</a>
+                    <div className={styles.tag}>
+                        <div className={styles.vl}></div>
+                        <a href={`/${item}`} key={index}>{item}</a>
+                    </div>
                 ))}
             </div>
         </div>
