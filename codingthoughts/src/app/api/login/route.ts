0aaf2b3e9a07c376040324a26aff564e8c230591
unknown <mypuzzle12@gmail.com>
2025-05-12 19:25:00 +0100

    created login api POST request and prismaClient to be used to communicate with database

---
+++ codingthoughts/src/app/api/login/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+import { prisma } from '../../lib/prismaClient/prismaClient';
+import { CookieListItem } from 'next/dist/compiled/@edge-runtime/cookies';
+
+export async function POST(req: Request) {
+    const {email, password} = await req.json();
+
+    const user = await prisma.user.findUnique({
+        where: { email },
+        include: { answers: true }
+    })
+
+    // check if the user can be found
+    if(!user) {
+        return NextResponse.json({ message: "User not found. Check Email spelling"}, { status: 400});
+    }
+
+    // check if the password is valid
+    const isValidPassword: boolean = user.password;
+
+    if(!isValidPassword) {
+        return NextResponse.json({ message: "Invalid Password!"}, { status: 401 });
+    }
+
+    // Create the response
+    const response = NextResponse.json({ message: "Logged in successfully!" });
+
+    // create a cookie with the userId to be used to retrieve answers
+    // specific to this user, and return this response
+    response.cookies.set("userId", user.id, {
+        httpOnly: true,
+        path: "/"
+    });
+
+    return response;
+}
