9da9dd983063e60d1e85b30948056c6e8eef24eb
unknown <mypuzzle12@gmail.com>
2025-05-15 19:28:59 +0100

    finished main styles for createform component

--- codingthoughts/src/components/form/CreateForm.tsx
+++ codingthoughts/src/components/form/CreateForm.tsx
@@ -9,34 +9,51 @@ export default function CreateForm() {
     const [language, setLanguage] = useState<string>("");
     const [description, setDescription] = useState<string>("");
     const [explanation, setExplanation] = useState<string>("");
+    const [link, setLink] = useState<string>("");
 
     const difficulties: string[] = ["easy", "medium", "hard"];
 
 
     return (
         <div className={styles.container}>
-            <header>CREATE YOUR ANSWER HERE</header>
-            <div>
-                <div>
-                    <input type="number" value={number} onChange={(e) => setNumber(e.target.value ? parseInt(e.target.value) : undefined)} placeholder="Problem Number..."></input>
-                    <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Problem name..."></input>
+            <header className={styles.main}>CREATE YOUR ANSWER HERE</header>
+            <div className={styles.nameContainer}>
+                <div className={styles.valueContainer}>
+                    <div className={styles.entryContainer}>
+                        <span>Problem Number</span>
+                        <input type="number" value={number} onChange={(e) => setNumber(e.target.value ? parseInt(e.target.value) : undefined)} placeholder="Problem Number..."></input>
+                    </div>
+                    <div className={styles.entryContainer}>
+                        <span>Problem Name</span>
+                        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Problem name..."></input>
+                    </div>
                 </div>
-                <div>
-                    <select>
+                <div className={styles.valueContainer}>
+                    <div className={styles.entryContainer}>
+                        <span>Difficulty</span>
+                        <select>
                         {difficulties.map((currentDifficulty, index) => (
                             <option key={index} onClick={() => {setDifficulty(currentDifficulty);}}>{currentDifficulty}</option>
                         ))}
-                    </select>
-                    <input type="text" value={language} placeholder="Enter language used here" onChange={(e) => {setLanguage(e.target.value);}}></input>
+                        </select>
+                    </div>
+                    <div className={styles.entryContainer}>
+                        <span>Language Used</span>
+                        <input type="text" value={language} placeholder="Enter language used here" onChange={(e) => {setLanguage(e.target.value);}}></input>
+                    </div>
                 </div>
             </div>
-            <div>
-                <span>Enter problem description here(optional)</span>
+            <div className={styles.detailsContainer}>
+                <span>Enter problem description here (optional)</span>
                 <textarea value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Description" rows={5}></textarea>
             </div>
-            <div>
+            <div className={styles.detailsContainer}>
                 <span>Enter problem explanation here</span>
                 <textarea value={explanation} onChange={(e) => setExplanation(e.target.value)} placeholder="Explanation" rows={10}></textarea>
+            </div>
+            <div className={styles.linkContainer}>
+                <span>Video link to solution (optional)</span>
+                <input type="text" value={link} onChange={(e) => setLink(e.target.value)} placeholder="Video Link"></input>
             </div>
             <button>CREATE</button>
         </div>
