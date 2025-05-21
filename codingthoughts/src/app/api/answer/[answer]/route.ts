ab09c1d492eeb817fbfce436631660a5eb0c4545
unknown <mypuzzle12@gmail.com>
2025-05-21 19:58:15 +0100

    finished writing page for any given answer, can now use go button on main page

--- codingthoughts/src/app/api/answer/[answer]/route.ts
+++ codingthoughts/src/app/api/answer/[answer]/route.ts
@@ -2,19 +2,38 @@ import { prisma } from "@/app/lib/prismaClient/prismaClient";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
-export async function GET(_: Request, { params }: { params: { answerName: string } }) {
+export async function GET(req: Request,{ params }: { params: { answer: string } }) {
     // get cookies to see if the user has logged in yet
     const userCookies = await cookies();
     const userId = userCookies.get("userId")?.value;
     if (!userId) {
         throw new Error("Not logged in yet");
     }
-    const {answerName} = params;
 
-    const answer = prisma.answer.findUnique({
+    // get the answer title and id from the url
+    const title: string = params.answer;
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get('id');
+
+    // check if the title or id are valid
+    if(!title || !id) {
+        return NextResponse.json({ message: "Invalid Parameters."}, { status: 400 });
+    }
+
+
+    // get the answer from the database
+    const answer = await prisma.answer.findUnique({
         where: {
+            id: parseInt(id),
             userId: userId,
-            title: answerName,
+            title: title,
         }
-    })
+    });
+
+    if(!answer) {
+        return NextResponse.json({ message: "Answer doesnt exist"}, { status: 400 });
+    }
+
+    // return the answer
+    return NextResponse.json(answer);
 }
--- codingthoughts/src/app/main/[answer]/page.tsx
+++ codingthoughts/src/app/main/[answer]/page.tsx
@@ -1,21 +1,48 @@
+"use client";
+
 import { checkAuth } from "@/app/lib/auth";
 import { Answer } from "@/app/lib/types/Answer";
-import { notFound } from "next/navigation";
 import { useEffect, useState } from "react";
+import { useSearchParams, useParams } from 'next/navigation';
 
 
-export default function Home({ params }: { params: { answer: string } }) {
-    const [answer, setAnswer] = useState<Answer>();
+export default function Home() {
+    // useState for answer and if answer was found
+    const [currentAnswer, setCurrentAnswer] = useState<Answer>();
     const [notFound, setNotFound] = useState<boolean>(false);
 
+    // get the id of answer from the search params
+    const searchParams = useSearchParams();
+    const id = searchParams.get('id');
+
+    // get anser from params
+    const params = useParams();
+
     useEffect(() => {
-        try {
-            const response = async() => {
-                await checkAuth();
+            const fetchAnswer = async() => {
+                try {
+                    await checkAuth();
+                    const response = await fetch(`/api/answer/${params.answer}?id=${id}`, {
+                        method: 'GET',
+                        credentials: 'include',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                    });
+
+                    if(!response.ok) {
+                        throw new Error("NOT FOUND");
+                    }
+                    
+                    const data = await response.json();
+                    alert(data);
+                    setCurrentAnswer(data);
+                } catch(error: any) {
+                    console.log(error.message);
+                    setNotFound(true);
+                }
             }
-        }catch( error: any) {
-            setNotFound(true);
-        }
+            fetchAnswer();
     }, []);
 
     if(notFound) return <div>404 PAGE NOT FOUND</div>;
