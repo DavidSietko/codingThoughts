55ee7ea082da48daee7ffa9db31f5987d3cdb1d8
unknown <mypuzzle12@gmail.com>
2025-06-05 15:37:12 +0100

    added resend to project to send emails

--- codingthoughts/src/app/Settings/page.tsx
+++ codingthoughts/src/app/Settings/page.tsx
@@ -1,11 +1,29 @@
 "use client";
 
 import InfoEntry from "@/components/InfoEntry/InfoEntry";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { checkAuth } from "../lib/auth";
 
 export default function Home() {
+    // boolean to see if user logged in or not
+    const [loggedIn, setLoggedIn] = useState<boolean>(false);
+
     const [username, setUsername] = useState<string>("");
     const [email, setEmail] = useState<string>("");
+
+    useEffect(() => {
+        const handler = async() => {
+            // see if user logged in and then fetch the username and email from the backend
+            try {
+                await checkAuth();
+                setLoggedIn(true);
+            } catch(error: any) {
+                console.log("User not logged in.");
+                setLoggedIn(false);
+            }
+        };
+        handler();
+    }, []);
 
     return (
         <div>
