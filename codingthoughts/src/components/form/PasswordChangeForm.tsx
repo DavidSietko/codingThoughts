c5f00a26bcf2fecdfb74dcd0dce285e624b156cc
unknown <mypuzzle12@gmail.com>
2025-06-12 12:26:42 +0100

    created updatePassword function in password change page to update password

---
+++ codingthoughts/src/components/form/PasswordChangeForm.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useState } from "react";
+import ErrorMessage from "./ErrorMessage";
+import { useRouter } from "next/navigation";
+
+export default function PasswordChangeForm() {
+    // useState for the error message and email
+    const [errorMessage, setErrorMessage] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+
+    // router to re-route user
+    const router = useRouter();
+
+    const sendEmail = async() => {
+        try {
+            // get response of sending email email
+            const response =  await fetch("/api/update/password", {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    email: email
+                })
+            });
+            // get data from the response
+            const data = await response.json();
+            // check if response ok
+            if(!response.ok) {
+                throw new Error(data.message);
+            }
+            // if ok response, redirect to homepage
+            router.push("/");
+        } catch(error: any) {
+            setErrorMessage(error.message);
+        }
+
+    }
+    return (
+        <form>
+            <div>
+                <p>Enter the email associated with your account</p>
+                <p>A new password will be sent to this email which you can use to log into your account</p>
+                <p>Upon a successfull email send, you will be logged out and sent to the home page</p>
+            </div>
+            <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter email here..."></input>
+            <ErrorMessage errorMessage={errorMessage} setErrorMessage={setErrorMessage}/>
+            <button onClick={sendEmail}>SEND</button>
+        </form>
+    );
+}
